15f63dab4b5096d889e5385e6ce0154812b4c630
AChen <2946517846@qq.com>
2026-08-23 22:20:35 +0800

    feat: add login scene flow

---
+++ Assets/Scripts/UI/UISetting/LogInStart.cs
@@ -0,0 +1,21 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class LogInStart : MonoBehaviour
+{
+    UIFrame m_uiFrame;
+
+    async UniTaskVoid Start()
+    {
+        UISettings loginSettings = await AddressableLoader.Instance.LoadUISettings(
+            AddressKeys.UISettings.LogInSetting);
+        if (loginSettings == null)
+        {
+            ALog.LogError("LogInSetting is null", "UI");
+            return;
+        }
+
+        m_uiFrame = loginSettings.CreateUIInstance();
+        m_uiFrame.OpenWindow(AddressKeys.Prefab.LogInWindow);
+    }
+}
