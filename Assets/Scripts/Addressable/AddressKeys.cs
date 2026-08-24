50de3fdc15016d65803889d23ca7ba0517729940
AChen <2946517846@qq.com>
2026-08-24 22:05:16 +0800

    整理文件夹

--- Assets/Scripts/Addressable/AddressKeys.cs
+++ Assets/Scripts/Addressable/AddressKeys.cs
@@ -10,8 +10,39 @@ public static class AddressKeys
     }
     public static class Sprite
     {
+        public static readonly string c_00 = "c_00";
+        public static readonly string c_01 = "c_01";
+        public static readonly string c_02 = "c_02";
+        public static readonly string c_03 = "c_03";
+        public static readonly string c_04 = "c_04";
+        public static readonly string c_05 = "c_05";
+        public static readonly string c_06 = "c_06";
+        public static readonly string c_07 = "c_07";
+        public static readonly string c_08 = "c_08";
+        public static readonly string c_09 = "c_09";
+        public static readonly string c_10 = "c_10";
+        public static readonly string w_00_Down = "w_00_Down";
+        public static readonly string w_00_Sprite = "w_00_Sprite";
         public static readonly string w_01_Down = "w_01_Down";
         public static readonly string w_01_Sprite = "w_01_Sprite";
+        public static readonly string w_02_Down = "w_02_Down";
+        public static readonly string w_02_Sprite = "w_02_Sprite";
+        public static readonly string w_03_Down = "w_03_Down";
+        public static readonly string w_03_Sprite = "w_03_Sprite";
+        public static readonly string w_04_Down = "w_04_Down";
+        public static readonly string w_04_Sprite = "w_04_Sprite";
+        public static readonly string w_05_Down = "w_05_Down";
+        public static readonly string w_05_Sprite = "w_05_Sprite";
+        public static readonly string w_06_Down = "w_06_Down";
+        public static readonly string w_06_Sprite = "w_06_Sprite";
+        public static readonly string w_07_Down = "w_07_Down";
+        public static readonly string w_07_Sprite = "w_07_Sprite";
+        public static readonly string w_08_Down = "w_08_Down";
+        public static readonly string w_08_Sprite = "w_08_Sprite";
+        public static readonly string w_09_Down = "w_09_Down";
+        public static readonly string w_09_Sprite = "w_09_Sprite";
+        public static readonly string w_10_Down = "w_10_Down";
+        public static readonly string w_10_Sprite = "w_10_Sprite";
     }
     public static class Scene
     {
